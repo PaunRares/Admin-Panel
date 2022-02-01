b77b071071dbd1488803db274f42901ba0711cff
Rares Paun <you@example.com>
2022-02-01 14:54:48 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,10 @@ const rootReducers = combineReducers({
     color: colorReducer,
     view: viewReducer
 })
-const middleware = [thunk, logger];
+const middleware = [thunk];
+if(process.env.NODE_ENV === 'development') {
+    middleware.push(logger)
+}
 
 const store = createStore(rootReducers, applyMiddleware(...middleware));
 
